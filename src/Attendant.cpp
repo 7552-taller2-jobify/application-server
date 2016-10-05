e1d897269975638b94f1a191c8bbf3ab5f0f14b0
Facundo <facundodefalco@hotmail.com.ar>
2016-10-05 17:23:14 -0300

    Changes to Attendant class

--- src/Attendant.cpp
+++ src/Attendant.cpp
@@ -3,30 +3,117 @@
 #include "Attendant.h"
 #include <string>
 
-Attendant::Attendant() {}
+Attendant::Attendant() {
+    this->methods = new std::vector<std::string>();
+}
 
 Attendant::~Attendant() {}
 
 void Attendant::attend(std::string operation) {}
 
-Login::Login() {}
+bool Attendant::isMethodSupported(std::string a_method) {
+    for (int i = 0; i < this->methods->size(); i++) {
+        if (this->methods->at(i) == a_method) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Login::Login() {
+    methods->push_back("GET");
+}
+
+Login::~Login() {}
 
 void Login::attend(std::string operation) {}
 
+
+
+RecoveryPass::RecoveryPass() {
+    // methods->push_back("GET");
+}
+
+RecoveryPass::~RecoveryPass() {}
+
 void RecoveryPass::attend(std::string operation) {}
+
+
+
+Contact::Contact() {
+    // methods->push_back("GET");
+}
+
+Contact::~Contact() {}
 
 void Contact::attend(std::string operation) {}
 
+
+
+Accept::Accept() {
+    // methods->push_back("GET");
+}
+
+Accept::~Accept() {}
+
 void Accept::attend(std::string operation) {}
+
+
+
+Reject::Reject() {
+    // methods->push_back("GET");
+}
+
+Reject::~Reject() {}
 
 void Reject::attend(std::string operation) {}
 
+
+
+ProfilePersonal::ProfilePersonal() {
+    // methods->push_back("GET");
+}
+
+ProfilePersonal::~ProfilePersonal() {}
+
 void ProfilePersonal::attend(std::string operation) {}
+
+
+
+ProfileSummary::ProfileSummary() {
+    // methods->push_back("GET");
+}
+
+ProfileSummary::~ProfileSummary() {}
 
 void ProfileSummary::attend(std::string operation) {}
 
+
+
+ProfileExpertise::ProfileExpertise() {
+    // methods->push_back("GET");
+}
+
+ProfileExpertise::~ProfileExpertise() {}
+
 void ProfileExpertise::attend(std::string operation) {}
 
+
+
+ProfileSkills::ProfileSkills() {
+    // methods->push_back("GET");
+}
+
+ProfileSkills::~ProfileSkills() {}
+
 void ProfileSkills::attend(std::string operation) {}
+
+
+
+ProfilePhoto::ProfilePhoto() {
+    // methods->push_back("GET");
+}
+
+ProfilePhoto::~ProfilePhoto() {}
 
 void ProfilePhoto::attend(std::string operation) {}
--- src/Attendant.h
+++ src/Attendant.h
@@ -4,8 +4,13 @@
 #define ATTENDANT_H_
 
 #include <string>
+#include <vector>
 
 class Attendant {
+ protected:
+    std::vector<std::string> *methods;
+    bool isMethodSupported(std::string a_method);
+
  public:
     Attendant();
     ~Attendant();
@@ -22,46 +27,64 @@ class Login: public Attendant {
 
 class RecoveryPass: public Attendant {
  public:
+    RecoveryPass();
+    ~RecoveryPass();
     void attend(std::string operation);
 };
 
 class Contact: public Attendant {
  public:
+    Contact();
+    ~Contact();
     void attend(std::string operation);
 };
 
 class Accept: public Attendant {
  public:
+    Accept();
+    ~Accept();
     void attend(std::string operation);
 };
 
 class Reject: public Attendant {
  public:
+    Reject();
+    ~Reject();
     void attend(std::string operation);
 };
 
 class ProfilePersonal: public Attendant {
  public:
+    ProfilePersonal();
+    ~ProfilePersonal();
     void attend(std::string operation);
 };
 
 class ProfileSummary: public Attendant {
  public:
+    ProfileSummary();
+    ~ProfileSummary();
     void attend(std::string operation);
 };
 
 class ProfileExpertise: public Attendant {
  public:
+    ProfileExpertise();
+    ~ProfileExpertise();
     void attend(std::string operation);
 };
 
 class ProfileSkills: public Attendant {
  public:
+    ProfileSkills();
+    ~ProfileSkills();
     void attend(std::string operation);
 };
 
 class ProfilePhoto: public Attendant {
  public:
+    ProfilePhoto();
+    ~ProfilePhoto();
     void attend(std::string operation);
 };
 
